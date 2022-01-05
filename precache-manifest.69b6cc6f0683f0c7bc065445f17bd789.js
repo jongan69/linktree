426fdaedebb4e5ada84b95be49b3c4fe0de93932
Jonathan Gan <jonny2298@live.com>
2022-01-04 20:32:39 -0500

    Updates

---
+++ precache-manifest.69b6cc6f0683f0c7bc065445f17bd789.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b821e427fac619352651e80abf346c02",
+    "url": "/linktree/index.html"
+  },
+  {
+    "revision": "cb068afec89b6329b810",
+    "url": "/linktree/static/css/main.7ec1b171.chunk.css"
+  },
+  {
+    "revision": "a9fb1d828739d563c232",
+    "url": "/linktree/static/js/2.29f9e395.chunk.js"
+  },
+  {
+    "revision": "a7044e965d556f20d857a40c8c0730b0",
+    "url": "/linktree/static/js/2.29f9e395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb068afec89b6329b810",
+    "url": "/linktree/static/js/main.11e0b65e.chunk.js"
+  },
+  {
+    "revision": "cf2a0ab90620c56c97f1",
+    "url": "/linktree/static/js/runtime-main.57bac9a6.js"
+  },
+  {
+    "revision": "874f98909122059c70b514a085dac955",
+    "url": "/linktree/static/media/github.874f9890.svg"
+  },
+  {
+    "revision": "bb982d137f7aad3d9d64c3c556b792ff",
+    "url": "/linktree/static/media/instagram.bb982d13.svg"
+  },
+  {
+    "revision": "67fdfcdc140b6b4a9229c48e905f954c",
+    "url": "/linktree/static/media/linkedin-in.67fdfcdc.svg"
+  },
+  {
+    "revision": "72541440ee2935f24f3b5c1911f853f0",
+    "url": "/linktree/static/media/tiktok.72541440.svg"
+  },
+  {
+    "revision": "285e53e00e2fa94944d7b58ed388bc42",
+    "url": "/linktree/static/media/youtube.285e53e0.svg"
+  }
+]);
