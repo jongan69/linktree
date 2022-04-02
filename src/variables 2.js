839a88bad07fdeefd0b381201db6a02747d1c375
Jonathan Gan <jonathan.gan@chain-bytes.com>
2022-04-02 10:16:56 -0400

    Added aome stuff

---
+++ src/variables 2.js
@@ -0,0 +1,13 @@
+export default {
+  textColor: '#fff',
+  facebookColor: '#4267B2',
+  instagramColor: '#C13584',
+  youtubeColor: '#FF0000',
+  whatsappColor: '#4AC959',
+  blogColor: '#FF585D',
+  linkedinColor: '#2867B2',
+  twitterColor: '#1DA1F2',
+  githubColor: '#24292e',
+  opt1Color: '#366251',
+  opt2Color: '#FF8300',
+}
