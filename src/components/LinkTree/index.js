965bfb5ae35145614ad6d27ae67fa7bbc82461d2
Jonathan Gan <jonathan.gan@chain-bytes.com>
2022-04-06 11:42:52 -0400

    added resume, wtf eric

--- src/components/LinkTree/index.js
+++ src/components/LinkTree/index.js
@@ -9,7 +9,7 @@ import PieChartIcon from '@mui/icons-material/PieChart';
 import PlayCircleFilledWhiteIcon from '@mui/icons-material/PlayCircleFilledWhite';
 import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
 import GitHubIcon from '@mui/icons-material/GitHub';
-
+import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
 
 
 // Other Options for links
@@ -25,10 +25,11 @@ export default function LinkTree() {
       <Header picture='https://avatars.githubusercontent.com/u/29899042?v=4' title='Jonathan Gan' subtitle='Junior Software Engineer & Investor.' />
       <Button link='https://www.linkedin.com/newsletters/red-dev-redemption-6915857887563030528/' icon={<ArticleIcon/>} name='Blog' backgroundColor={variables.githubColor} />
       <Button link='https://expo.dev/@jongan69' icon={<AppsIcon/>} name=' Mobile Apps' backgroundColor={variables.githubColor} />
+      <Button link='https://github.com/jongan69' icon={<GitHubIcon/>} name='Github' backgroundColor={variables.githubColor} />
+      <Button link='https://drive.google.com/file/d/1JkFOGhQRhGYG5iN_FxazbL6G5MZnmK3S/view?usp=sharing' icon={<AssignmentIndIcon/>} name=' Resume' backgroundColor={variables.githubColor} />
       <Button link='https://share.public.com/jonngan' icon={<PieChartIcon/>} name='Public' backgroundColor={variables.twitterColor} />
       <Button link='https://www.youtube.com/channel/UCj2qx1zXvPtzg-MDADw1kOQ' icon={<PlayCircleFilledWhiteIcon/>} name='Youtube' backgroundColor={variables.youtubeColor} />
       <Button link='https://godsunchained.com/account/register?referral=CeGtiPFBzc' icon={<SportsEsportsIcon/>} name='Gods Unchained' backgroundColor={variables.linkedinColor} />
-      <Button link='https://github.com/jongan69' icon={<GitHubIcon/>} name='Github' backgroundColor={variables.githubColor} />
     </Container>
   )
 }
