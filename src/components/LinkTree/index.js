398aa0dc526339fba8ef452a6d633e5c5771522d
Jonathan Gan <jonathan.gan@chain-bytes.com>
2022-07-03 03:34:05 -0400

    Pushing to IPFS

--- src/components/LinkTree/index.js
+++ src/components/LinkTree/index.js
@@ -13,6 +13,7 @@ import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
 import ParticlesBg from 'particles-bg'
 
 // Other Options for links
+const gitcoinLogo = require('../../images/negative.svg');
 // const instagramLogo = require('../../images/instagram.svg');
 // const linkedinLogo = require('../../images/linkedin-in.svg');
 // const tiktokLogo = require('../../images/tiktok.svg');
@@ -38,24 +39,24 @@ export default function LinkTree() {
     g: 5,    // gravity
     // f: [2, -1], // force
     onParticleUpdate: (ctx, particle) => {
-        ctx.beginPath();
-        ctx.rect(particle.p.x, particle.p.y, particle.radius * 2, particle.radius * 2);
-        ctx.fillStyle = particle.color;
-        ctx.fill();
-        ctx.closePath();
+      ctx.beginPath();
+      ctx.rect(particle.p.x, particle.p.y, particle.radius * 2, particle.radius * 2);
+      ctx.fillStyle = particle.color;
+      ctx.fill();
+      ctx.closePath();
     }
   };
 
   return (
     <Container>
-      <Header picture='https://avatars.githubusercontent.com/u/29899042?v=4' title='Jonathan Gan' subtitle='Junior Software Engineer & Investor.' />
-      <Button link='https://www.linkedin.com/newsletters/red-dev-redemption-6915857887563030528/' icon={<ArticleIcon/>} name='Blog' backgroundColor={variables.githubColor} />
-      <Button link='https://expo.dev/@jongan69' icon={<AppsIcon/>} name=' Mobile Apps' backgroundColor={variables.githubColor} />
-      <Button link='https://github.com/jongan69' icon={<GitHubIcon/>} name='Github' backgroundColor={variables.githubColor} />
-      <Button link='https://docs.google.com/document/d/1euLUOVIyY6zYMQ2dA6sXCq0qt7qhCNT5zu-c7GzX0s0/edit?usp=sharing' icon={<AssignmentIndIcon/>} name=' Resume' backgroundColor={variables.githubColor} />
-      <Button link='https://share.public.com/jonngan' icon={<PieChartIcon/>} name='Public' backgroundColor={variables.twitterColor} />
-      <Button link='https://www.youtube.com/channel/UCj2qx1zXvPtzg-MDADw1kOQ' icon={<PlayCircleFilledWhiteIcon/>} name='Youtube' backgroundColor={variables.youtubeColor} />
-      <Button link='https://godsunchained.com/account/register?referral=CeGtiPFBzc' icon={<SportsEsportsIcon/>} name='Gods Unchained' backgroundColor={variables.linkedinColor} />
+      <Header picture='https://avatars.githubusercontent.com/u/29899042?v=4' title='Jonathan Gan' subtitle='Software Engineer & Investor.' />
+      <Button link='https://www.linkedin.com/newsletters/red-dev-redemption-6915857887563030528/' icon={<ArticleIcon />} name='Blog' backgroundColor={variables.githubColor} />
+      <Button link='https://expo.dev/@jongan69' icon={<AppsIcon />} name=' Mobile Apps' backgroundColor={variables.githubColor} />
+      <Button link='https://github.com/jongan69' icon={<GitHubIcon />} name='Github' backgroundColor={variables.githubColor} />
+      <Button link='https://gitcoin.co/jongan69' icon={<img src={gitcoinLogo} />} backgroundColor={variables.githubColor} />
+      <Button link='https://docs.google.com/document/d/1euLUOVIyY6zYMQ2dA6sXCq0qt7qhCNT5zu-c7GzX0s0/edit?usp=sharing' icon={<AssignmentIndIcon />} name=' Resume' backgroundColor={variables.githubColor} />
+      <Button link='https://share.public.com/jonngan' icon={<PieChartIcon />} name='Public' backgroundColor={variables.twitterColor} />
+      <Button link='https://www.youtube.com/channel/UCZjg1Y92vjGDxaHA21WpDLg' icon={<PlayCircleFilledWhiteIcon />} name='Youtube' backgroundColor={variables.youtubeColor} />
       <ParticlesBg type="random" config={config} bg={true} />
     </Container>
   )
